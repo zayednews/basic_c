fc22891128d866ceaed73a8dd8a1fd6063cecd0e
Zayed Bin Maleque (News) <ne.zayed@gmail.com>
2018-03-15 13:05:38 +0600

    Update macroDefineSquare.c

--- macroDefineSquare.c
+++ macroDefineSquare.c
@@ -7,3 +7,14 @@ int main()
   return 0;
 }
 // Output: 1
+
+//another way
+
+#define square(x) x*x
+int main()
+{
+  int x = 36/square(6); // Expended as 36/6*6
+  printf("%d", x);
+  return 0;
+}
+// Output: 36
