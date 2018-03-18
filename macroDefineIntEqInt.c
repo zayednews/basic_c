8fcf2cc0d5ad6c5bb89a595285ee91d2fead84e9
Zayed Bin Maleque (News) <ne.zayed@gmail.com>
2018-03-18 10:18:22 +0600

    Update macroDefineIntEqInt.c

--- macroDefineIntEqInt.c
+++ macroDefineIntEqInt.c
@@ -4,6 +4,12 @@ int main()
 {
    int MAX = 100;
    //error
+  /* int main()
+   {
+      int 1000 = 100;  // COMPILER ERROR: expected unqualified-id before numeric constant
+      printf("%d ", 1000);
+      return 0;
+   }*/
    printf("%d ", MAX);
    return 0;
 }
